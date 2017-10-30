6ec7896808142572d9b1aa96027fb6e7e43ad9b5
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2017-10-30 21:03:31 +0100

    [wip] add first draft of full problem solution

---
+++ taio/solution.py
@@ -0,0 +1,34 @@
+from .graph import (
+    create_graph,
+    edmonds_karp,
+)
+from .data_loader import DataLoader
+
+
+def associate_worker_with_project(flow_graph):
+    labels_modifier = flow_graph.graph['labels_modifier']
+    task_info = flow_graph.graph['task_info']
+    for worker_id in range(task_info.workers_number):
+        worker_label = labels_modifier.modify('workers', worker_id)
+        for feature, edge_data in flow_graph.adj[worker_label].items():
+            if edge_data['flow'] > 0:
+                for project, edge_data in flow_graph.adj[feature].items():
+                    if edge_data['flow'] > 0:
+                        # TODO: decode project label to project_number
+                        yield (worker_id, project)
+                        flow_graph[feature][project]['flow'] -= 1
+                        break
+                break
+
+
+def dev_solution(test_data_path):
+    task = DataLoader().load(test_data_path)
+    g = create_graph(task, labels_modifiers={
+        'workers': lambda x: f'w_{x}',
+        'features': lambda x: f'f_{x}',
+        'projects': lambda x: f'p_{x}',
+        'source': lambda x: 'S',
+        'target': lambda x: 'T',
+    })
+    flow_graph = edmonds_karp(g, g.graph['source'], g.graph['target'])
+    return list(associate_worker_with_project(flow_graph))
