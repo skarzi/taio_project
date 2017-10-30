f1060eb57d099b0ff66d27f9306f3d49529074bc
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2017-10-30 21:00:31 +0100

    add Edmonds-Karp algorithm implementation

--- taio/graph/__init__.py
+++ taio/graph/__init__.py
@@ -1,4 +1,6 @@
 from .drawer import GraphDrawer
+from .custom_graph import create_graph
+from .edmonds_karp import edmonds_karp
 from .labels_modifier import LabelsModifier
 
-__all__ = ['GraphDrawer', 'LabelsModifier']
+__all__ = ['GraphDrawer', 'LabelsModifier', 'edmonds_karp', 'create_graph']
---
+++ taio/graph/edmonds_karp.py
@@ -0,0 +1,48 @@
+import collections
+
+
+def residual_capacity(G, u, v):
+    return G[u][v]['capacity'] - G[u][v]['flow']
+
+
+def edmonds_karp(G, s, t):
+    g = G.copy()
+    for u, v in G.edges:
+        g[u][v]['flow'] = 0
+        if not G.has_edge(v, u):
+            g.add_edge(v, u, capacity=0, flow=0)
+
+    max_flow = 0
+    path = {node: None for node in g}
+
+    while bfs(g, s, t, path):
+        path_flow = float('inf')
+        v = t
+        while v != s:
+            path_flow = min(path_flow, residual_capacity(g, path[v], v))
+            v = path[v]
+        max_flow += path_flow
+        v = t
+        while v != s:
+            u = path[v]
+            g[u][v]['flow'] += path_flow
+            g[v][u]['flow'] = -g[u][v]['flow']
+            v = u
+    g.graph['max_flow'] = max_flow
+    return g
+
+
+def bfs(G, s, t, path):
+    visited = {node: False for node in G}
+    queue = collections.deque()
+    queue.append(s)
+    visited[s] = True
+
+    while queue:
+        u = queue.popleft()
+        for v in G[u]:
+            if residual_capacity(G, u, v) > 0 and visited[v] is False:
+                queue.append(v)
+                visited[v] = True
+                path[v] = u
+    return visited[t]
