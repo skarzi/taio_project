13842e34e485e090cf9efe4604dfcf849fd88a0d
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2017-11-03 13:48:41 +0100

    improve dev_solution
    If debug is  True, then show final flow graph.

--- taio/solution.py
+++ taio/solution.py
@@ -1,6 +1,7 @@
 from .graph import (
     create_graph,
     edmonds_karp,
+    GraphDrawer,
 )
 from .data_loader import DataLoader
 
@@ -23,8 +24,19 @@ def associate_worker_with_project(flow_graph):
                 break
 
 
-def dev_solution(test_data_path):
+def dev_solution(test_data_path, debug=False):
     task = DataLoader().load(test_data_path)
     g = create_graph(task)
     flow_graph = edmonds_karp(g, g.graph['source'], g.graph['target'])
+    if debug:
+        graph_drawer = GraphDrawer()
+        graph_drawer.draw(
+            flow_graph,
+            label_edge=lambda x: (
+                x[:2],
+                f"{x[-1]['flow']}/{x[-1]['capacity']}",
+            ),
+            edge_list=g.edges,
+        )
+        return g, flow_graph, list(associate_worker_with_project(flow_graph))
     return list(associate_worker_with_project(flow_graph))
