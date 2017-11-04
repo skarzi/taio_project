6b1aeeeba49e1c800ea6228b113b0fbc55c21b99
barteksielicki <barteksielicki@gmail.com>
2017-11-04 22:12:58 +0100

    don't use graph drawer

--- taio/solution.py
+++ taio/solution.py
@@ -1,9 +1,8 @@
+from .data_loader import DataLoader
 from .graph import (
     create_graph,
     edmonds_karp,
-    GraphDrawer,
 )
-from .data_loader import DataLoader
 
 
 def associate_worker_with_project(flow_graph):
@@ -24,19 +23,8 @@ def associate_worker_with_project(flow_graph):
                 break
 
 
-def dev_solution(test_data_path, debug=False):
+def solution(test_data_path):
     task = DataLoader().load(test_data_path)
     g = create_graph(task)
     flow_graph = edmonds_karp(g, g.graph['source'], g.graph['target'])
-    if debug:
-        graph_drawer = GraphDrawer()
-        graph_drawer.draw(
-            flow_graph,
-            label_edge=lambda x: (
-                x[:2],
-                f"{x[-1]['flow']}/{x[-1]['capacity']}",
-            ),
-            edge_list=g.edges,
-        )
-        return g, flow_graph, list(associate_worker_with_project(flow_graph))
     return list(associate_worker_with_project(flow_graph))
